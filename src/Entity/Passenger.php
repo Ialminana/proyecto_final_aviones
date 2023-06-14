c0480edfa35cfb8bacdf5e8fd059273ed82fe3b5
Ialminana <inma.alminana82@gmail.com>
2023-06-14 18:59:37 +0200

    mmm

--- src/Entity/Passenger.php
+++ src/Entity/Passenger.php
@@ -3,13 +3,11 @@
 namespace App\Entity;
 
 use App\Repository\PassengerRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PassengerRepository::class)]
-class Passenger 
+class Passenger extends Person
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -18,14 +16,6 @@ class Passenger
 
     #[ORM\Column]
     private ?int $seat = null;
-
-    #[ORM\ManyToMany(targetEntity: Flight::class, inversedBy: 'passengers')]
-    private Collection $flight;
-
-    public function __construct()
-    {
-        $this->flight = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -40,30 +30,6 @@ class Passenger
     public function setSeat(int $seat): static
     {
         $this->seat = $seat;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Flight>
-     */
-    public function getFlight(): Collection
-    {
-        return $this->flight;
-    }
-
-    public function addFlight(Flight $flight): static
-    {
-        if (!$this->flight->contains($flight)) {
-            $this->flight->add($flight);
-        }
-
-        return $this;
-    }
-
-    public function removeFlight(Flight $flight): static
-    {
-        $this->flight->removeElement($flight);
 
         return $this;
     }
